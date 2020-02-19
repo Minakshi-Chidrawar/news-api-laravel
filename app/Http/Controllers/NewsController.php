ccb05a07196fd3214f34ca9da7c2d2e997b33bd8
minakshi-chidrawar <minakshichidrawar@gmail.com>
2020-02-19 21:12:53 +0000

    Amended to work with get and post requests

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -11,12 +11,29 @@ class NewsController extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  NewsService               $newsService
      * @return \Illuminate\Http\Response
      */
     public function __invoke(Request $request, NewsService $newsService)
     {
-        $headlines = $newsService->headlines();
+        $response['newsSources'] = $newsService->allSources();
 
-        dd($headlines);
+        if ($request->isMethod('get')) {
+            $response['sourceName'] = config('news.default_news_source');
+            $response['sourceId'] = config('news.default_news_source_id');
+
+        } else {
+            $request->validate([
+                'source' => 'required|string',
+            ]);
+
+            $split_input = explode(':', $request->source);
+            $response['sourceId'] = trim($split_input[0]);
+            $response['sourceName'] = trim($split_input[1]);
+        }
+
+        $response['news'] = $newsService->headlines($response['sourceId']);
+
+        return view('news', compact('response'));
     }
 }
