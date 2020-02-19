aae7056fa0598e981ac36f9c561bbee736bd2253
minakshi-chidrawar <minakshichidrawar@gmail.com>
2020-02-19 21:16:18 +0000

    Changed URL for the newsapi

--- config/news.php
+++ config/news.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    'base_uri' => env('NEWS_API_BASE_URI', 'https://newsapi.org/v2/'),
+    'base_uri' => env('NEWS_API_BASE_URI', 'https://newsapi.org/v1/'),
     'api_token' => env('NEWS_API_TOKEN'),
+    'default_news_source' => env('DEFAULT_NEWS_SOURCE', 'CNN'),
+    'default_news_source_id' => env('DEFAULT_NEWS_SOURCE_ID', 'cnn'),
 ];
